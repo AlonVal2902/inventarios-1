5268623bf7e25b5e0847bb44551d925820ec8055
unknown <edgaralonso.2902@gmail.com>
2024-08-01 11:24:55 -0500

    loginCorrecion

--- src/components/organismos/FooterLogin.jsx
+++ src/components/organismos/FooterLogin.jsx
@@ -5,18 +5,14 @@ export function FooterLogin() {
     <Container>
       <section className="lock">
         <GiPadlock />
-        <span>
-          Esta es una página segura de codigo369. Si tienes dudas sobre la
-          autenticidad de la web, comunícate con
-          <br /> nosotros al 311-9898 o a través de nuestros medios digitales.
-        </span>
+   
       </section>
       <section className="derechos">
-        <span>StockPRO S.A - RUC: 20100047218</span>
+        <span>CAVTEX S.A.C - RUC: 20521828243</span>
         <div className="separador"></div>
         <span>Todos los derechos reservados</span>
         <div className="separador"></div>
-        <span>© 2023 codigo369.com</span>
+        <span>© 2024 </span>
       </section>
     </Container>
   );
@@ -46,6 +42,7 @@ const Container = styled.div`
    }
     span{
       margin-top:5px;
+      margin-left:12px;
     }
   }
 `;
